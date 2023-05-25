45cfe0c543fa6be668dcd9c0abac3e854c41fa9f
Alin Grigorescu <alin.grig87@gmail.com>
2023-05-25 20:20:57 +0300

    Students table from API

--- springboot-react-catalog/catalog/src/ui/src/App.jsx
+++ springboot-react-catalog/catalog/src/ui/src/App.jsx
@@ -6,33 +6,34 @@ import './App.css'
 
 function App() {
   const [students, setStudents] = useState([]);
-  const [users, setUsers] = useState([]);
 
   function getStudentsFromAPI() {
     axios.get("http://localhost:8080/students")
         .then(students => setStudents(students.data))
   }
 
-    function getGitHubUsersFromAPI() {
-      axios.get("https://api.github.com/users")
-          .then(users => setUsers(users.data))
-    }
-
-//   useEffect(getStudentsFromAPI, []);
-  useEffect(getGitHubUsersFromAPI, []);
-
-
-
+  useEffect(getStudentsFromAPI, []);
 
   return (
     <div className="users">
-
-{/*        {students.map((student,i) => <h1 key={i}>{student.firstName} {student.lastName}</h1>)} */}
-       {users.map((user,i) =>
-       <div key={i}>
-            <img src={user.avatar_url} width={60}/>
-            <h5>{user.login}</h5>
-       </div>)}
+        <table className="students">
+            <thead>
+                <th>FirstName</th>
+                <th>Last Name</th>
+                <th>Grades</th>
+                <th>Average</th>
+            </thead>
+            <tbody>
+       {students.map((student,i) =>
+                        <tr key={i}>
+                            <td>{student.firstName}</td>
+                            <td>{student.lastName}</td>
+                            <td>{student.grades.join(',')}</td>
+                            <td>{student.average}</td>
+                        </tr>)
+       }
+            </tbody>
+        </table>
 
     </div>
   )
